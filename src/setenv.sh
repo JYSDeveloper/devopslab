3722e3f9ad206f87834768a8b07f5bbb8823bb98
JYSDeveloper <yusheng.jiang@qq.com>
2019-12-07 14:02:15 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT= JYSDeveloper
 
 export MYCLUSTER=tektonknativebeijing25
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AC/.bluemix/plugins/container-service/clusters/tektonknativebeijing25/kube-config-tok02-tektonknativebeijing25.yml
 
 export NAMESPACE=tektonknativebeijing25
 export EMAIL= yusheng.jiang@qq.com
-export INGRESS=
+export INGRESS=tektonknativebeijing25-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
